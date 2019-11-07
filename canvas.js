02dd7d36629ba6550ae0b32207246f38bb926f63
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-11-07 17:35:26 +0400

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,17 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+// for line
+// ctx.moveTo(0, 0);
+// ctx.lineTo(400, 200);
+// ctx.stroke();
+
+// ctx.moveTo(400, 0);
+// ctx.lineTo(0, 200);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.arc(200, 100, 50, Math.PI / 2, 3 * Math.PI / 2);
+// ctx.fill();
+
+ctx.fillRect(10, 10, 380, 180);
